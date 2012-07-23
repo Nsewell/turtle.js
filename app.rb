a045a7774b432632acf862c56bb849a0c5808c00
Nick Sewell <nsewell@gmail.com>
2012-07-23 10:30:58 +1000

    Added iframe embed series with a bunch of hacks

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'pp'
 require 'haml' #Tilt threads
+require 'json'
 
-require 'model'
+require './model'
 
 
 configure do
@@ -11,7 +12,6 @@ end
 
 get '/' do
   @code = ""
-	@hash = ''
 
   @recent = Saves.all
   
@@ -38,6 +38,68 @@ get '/images/:id' do
 	"<img src=\"data:image/png;base64,#{s.img}\">"
 end
 
+get '/koch/:id' do
+  s = JSON.parse(File.read('examples/koch.txt'))
+  i = params[:id].to_i
+  @code = s[i]["code"]
+  @text = s[i]["text"]
+  @recent = Saves.all
+  haml :index
+end
+
+get '/user/:id' do
+  @recent = Saves.find{|f| f.user == params[:id].to_i} || []
+  haml :index
+end
+
+get '/bh/:id' do
+  s = JSON.parse(File.read('examples/harvey.json'))
+  @text = s[params[:id].to_i]["text"]
+  #@text = ""
+  @code = s[params[:id].to_i]["code"]
+  haml :embed
+end
+
+get '/examples/:id' do
+  s = JSON.parse(File.read('examples/koch.json'))["examples"]["Koch Snowflake"]
+  @text = s[params[:id].to_i]["text"]
+  @code = s[params[:id].to_i]["code"]
+  haml :embed
+end
+
+get '/series' do
+  s = JSON.parse(File.read('examples/harvey.json'))
+  @recent = s
+  @series = 'bh'
+  haml :series
+end
+
+get '/examples' do
+  s = JSON.parse(File.read('examples/koch.json'))["examples"]["Koch Snowflake"]
+  @recent = s
+  @series = 'examples'
+  haml :series
+end
+
+get '/embed/:id' do
+  s = Saves.find{|f| f.id == params[:id].to_i}
+  if s.nil?
+    @code = @text = ""
+  else
+    @code = s["code"]
+    @text = ""
+  end
+  haml :embed, :format => :html5
+end
+
+get '/test' do
+  haml :canvas_lines
+end
+get '/test2' do
+  @recent = Saves
+  haml :test
+end
+
 get '/:id' do
   s = Saves.find{|f| f.id == params[:id].to_i}
   if s.nil?
@@ -53,6 +115,6 @@ get '/:id' do
   haml :index
 end
 
-
-get 'static' do
+get '/help.txt' do
+  File.read('public/help.txt')
 end
